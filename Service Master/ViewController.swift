c64d20c9d279e02e32599d3130cda6dfc5664cd1
Swapnil <swarmandal.swapnil@gmail.com>
2022-11-30 19:03:37 -0500

    Initial Commit

---
+++ Service Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Service Master
+//
+//  Created by Swapnil Roy Chowdhury on 2022-11-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
